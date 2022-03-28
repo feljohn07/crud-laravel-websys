9990c23df3509b37b73253a2749cb86509cc6a0c
Fel John <feljohn.loe.bangasan@gmail.com>
2022-03-28 22:47:25 +0800

    update

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,11 +7,10 @@
     <title>Clinic Appointment</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/jquery-3.6.0.js') }}" defer></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/jquery-3.6.0.js') }}" defer></script>
-    
 
 </head>
 
